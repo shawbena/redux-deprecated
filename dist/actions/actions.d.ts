18546bf22cfcc3004114d8f3de98e79b2b8f494b
shawbena <shawbena@outlook.com>
2018-04-11 22:03:38 +0800

    commit for merge

--- dist/actions/actions.d.ts
+++
@@ -1,5 +0,0 @@
-import { AnyAction } from 'redux';
-import { VisibilityFilters } from "./actionTypes";
-export declare function addTodo(text: string): AnyAction;
-export declare function toggleTodo(index: number): AnyAction;
-export declare function setVisibiltyFilter(filter: VisibilityFilters): AnyAction;
--- docs/react-redux.md
+++ docs/react-redux.md
@@ -4,3 +4,4 @@ __<Provider store>__
 
 使下面组件层级中的 `connect()` 调用可访问 Redux store. 正常如果你不用 `<Provider>` 包括父组件或祖先组件便不能使用 `connect()`.
 
+如果真的需要，你可以手动地
